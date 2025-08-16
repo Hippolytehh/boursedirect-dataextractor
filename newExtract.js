425cf62e463ad57fc1e067c8eb6efcac844ae341
hippolyteh <frdistermin32@gmail.com>
2025-08-16 17:53:59 +0200

    add to browser memory

--- newExtract.js
+++ newExtract.js
@@ -7,7 +7,7 @@ const urls = Array.from({ length: lastYear - firstYear + 1 }, (_, i) => firstYea
       .flatMap(month =>
         Array.from({ length: 31 }, (_, i) => String(i + 1).padStart(2, '0'))
           .map(day =>
-            `https://www.boursedirect.fr/priv/new/releveOpe.php?nc=1&type=RO&year=${year}&month=${month}&day=${day}&trash=/avis.pdf`
+            `https://www.boursedirect.fr/priv/new/releveOpe.php?nc=2&type=RO&year=${year}&month=${month}&day=${day}&trash=/avis.pdf`
           )
       )
   );
@@ -18,11 +18,17 @@ const testUrls = async (urlsToTest) => {
       try {
         const response = await fetch(url, { method: 'GET' }); // Use HEAD to just get headers
         const html = await response.text();
-        return { url, status: response.status, html: html};
+        return { url, status: response.status, hasReleve: html.startsWith("Erreur fatale veuillez contacter le support Capitol") ? false : true };
       } catch (error) {
         return { url, error: error.message };
       }
     })
   );
-  console.log(results);
+  const hasReleveResults = results.filter(result => result.hasReleve);
+  return hasReleveResults;
 };
+
+// Write to browser memory
+await testUrls(urls).then(r).then(r => {
+    localStorage.setItem('releves', JSON.stringify(r));
+})
